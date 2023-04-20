54162693509cfd78066f77a2b0f4b8b6cdca0267
brunoholanda <holanda_rodrigues@hotmail.com>
2023-04-20 17:41:07 -0300

    inicio da conversao de js para ts

---
+++ src/componentes/Banner/index.tsx
@@ -0,0 +1,21 @@
+import './Banner.css'
+
+import React from 'react';
+
+interface BannerProps {
+    enderecoImagem: string
+    textoAlternativo?: string //a interrogacao aqui me informa que o texto alternativo e opcional
+}
+
+export const Banner = ({enderecoImagem, textoAlternativo}:BannerProps) => {
+    // JSX
+    return (
+        <header className="banner">
+            {/*<img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>*/}
+            <img src={enderecoImagem} alt={textoAlternativo}/>
+
+        </header>
+    )
+}
+
+export default Banner;
